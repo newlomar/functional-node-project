e8a3ef04a51bf2b62a108239c8b197b666bd2f7e
Newton Lomar <newtonlomar16@gmail.com>
2022-05-05 08:17:05 -0300

    adding configuration of post, put and delete routes

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import { v4 as uuid } from "uui";
+
 const app = express();
 
 app.use(express.json());
@@ -9,17 +11,50 @@ interface User {
   email: string;
 }
 
-const users: USer[] = [];
+const users: User[] = [];
 
 app.get("/users", (request, response) => {
   return response.json(users);
 });
 
-app.post("/users", (request, response) => {});
+app.post("/users", (request, response) => {
+  const { name, email } = request.body;
 
-app.put("/users/:id", (request, response) => {});
+  const user = { id: uuid(), name, email };
 
-app.delete("/users", (request, response) => {});
+  users.push(user);
+
+  return response.json(user);
+});
+
+app.put("/users/:id", (request, response) => {
+  const { id } = request.params;
+  const { name, email } = request.body;
+
+  const userIndex = users.findIndex((user) => user.id === id);
+
+  if (userIndex < 0) {
+    return response.status(404).json({ error: "User not found." });
+  }
+
+  const user = { id, name, email };
+
+  users[userIndex] = user;
+
+  return response.json(user);
+});
+
+app.delete("/users/:id", (request, response) => {
+  const { id } = request.params;
+
+  const userIndex = users.findIndex((user) => user.id === id);
+
+  if (userIndex < 0) {
+    return response.status(404).json({ error: "User not found." });
+  }
+
+  users.splice(userIndex, 1);
+});
 
 app.listen("3333", () => {
   console.log("Back-end Started!");
