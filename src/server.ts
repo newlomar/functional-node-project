bc99e38471afa1b57ed1fa626a9dc7730e6a577f
Newton Lomar <newtonlomar16@gmail.com>
2022-05-05 08:07:26 -0300

    adding basic routes

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,17 @@
 import express from "express";
-
 const app = express();
+
+app.use(express.json());
+
+const users = [];
+
+app.get("/users", (request, response) => {});
+
+app.post("/users", (request, response) => {});
+
+app.put("/users/:id", (request, response) => {});
+
+app.delete("/users", (request, response) => {});
 
 app.listen("3333", () => {
   console.log("Back-end Started!");
