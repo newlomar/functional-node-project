54cb91577a82ea81994be75faa27b9a3de75bba3
Newton Lomar <newtonlomar16@gmail.com>
2022-05-08 10:16:13 -0300

    adding and configuring cors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
 import { v4 as uuid } from "uuid";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors({ origin: "*" }));
 
 interface User {
   id: string;
